1ee67c39cc3bf4ea55244d953dd4ae1e63cbd72d
dyllis <dev.dyllis@gmail.com>
2021-01-06 13:15:15 +0900

    - init project

---
+++ src/main/java/dev/dyllis/stairs/StairsApplication.java
@@ -0,0 +1,13 @@
+package dev.dyllis.stairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StairsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StairsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/dyllis/stairs/StairsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dyllis.stairs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StairsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
